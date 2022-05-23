bbf90f0a6a86ba0a8d366c9dd1f7d30954944293
Wandir Pereira Filho <wandir@gmail.com>
2022-05-23 09:22:10 -0300

    Incializando repositório

---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ListadoPacientes = () => {
+  return (
+    <div className="w-1/2">
+       <h2>Listado Pacientes</h2>
+    </div>
+  )
+}
+
+export default ListadoPacientes
